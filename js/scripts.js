488f026ef6f18cea5e5283ea738b3d2d60ed0b04
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-04-19 11:05:31 -0500

    Update scripts.js
    removed typo

--- js/scripts.js
+++ js/scripts.js
@@ -62,7 +62,7 @@ function newItem(){
 //3(i). Adding the delete button "X":
   let crossOutButton=$('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
-  li.append.(crossOutButton);
+  li.append(crossOutButton);
       
 //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   function deleteListItem(){
