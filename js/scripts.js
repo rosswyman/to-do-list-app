c181ecb82c8834d3d89e9952bc059ab9c50782af
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-04-19 10:53:01 -0500

    Revert "Update scripts.js"
    This reverts commit 43ebe25d52f3891d44cd33ea6dc989bb44dc5a4b.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,19 @@
 function newItem(){
 
-
-    // //1. Adding a new item to the list of items: 
-    // //javascript   
-    // let li = document.createElement("li");
-    // let inputValue = document.getElementById("input").value;
-    // let text = document.createTextNode(inputValue);
-    // li.appendChild(text);
-
-    // if (inputValue === '') {
-    //   alert("You must write something!");
-    // } else {
-    //   let list = document.querySelector('#list');
-    //   list.appendChild(li);
-    // }
-
-    // jQuery
-    let inputValue=$('#input').val;
-    let list=$('#list');
-    let li=$('<li></li>');
-
-    // Assign value of input field to new li
-    li.append(inputValue);
-
-    // Check for empty input
-    if(inputValue===''){
-      alert('You forgot to add a task.  Please enter a new item.')
-    } else {
-    // add new item to list
-    list.append(li);
-  }
-
+    //javascript
+    //1. Adding a new item to the list of items: 
+       let li = document.createElement("li");
+       let inputValue = document.getElementById("input").value;
+       let text = document.createTextNode(inputValue);
+       li.appendChild(text);
+    
+       if (inputValue === '') {
+         alert("You must write something!");
+       } else {
+         let list = document.querySelector('#list');
+         list.appendChild(li);
+       }
+    
      //2. Crossing out an item from the list of items:
        function crossOut() {
              li.classList.toggle("strike");
@@ -51,14 +34,9 @@ function newItem(){
      // 4. Reordering the items: 
        $('#list').sortable();
     
-}
+    }
     
-// // jquery
-// // 1. Adding a new item to the list of items
-// function newItem(){
-  
-
-// }
+     
     
     
     
